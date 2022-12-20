5b0b7cdb0fa58cc6dc95abe505e0921f5bb6a32e
MARCIO TARGINO <105668116+marciot1@users.noreply.github.com>
2022-12-20 19:02:10 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   double b = double.Parse()
+   double a = double.
+  }
+}
+class Retangulo {
+  public static void Main (string[] args) {
+    private void double disatncia;
+    private double peso;
+  }
+}
